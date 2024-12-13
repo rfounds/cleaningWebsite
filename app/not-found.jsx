d89b9ab26fc954fb40cd94a5db01b15ce9f9d3f8
rfounds <49726560+rfounds@users.noreply.github.com>
2024-12-13 12:30:08 -0600

    Update not-found.jsx

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 export const metadata = {
-    title: 'Not Found || Deski-Saas & Software Nextjs Template',
-    description: `Unleash the full potential of your software projects with our SaaS-focused Next.js template. Create engaging blogs, showcase your talented team, set up an online shop, provide helpful FAQs, and offer exceptional services with our feature-rich solution. Elevate your SaaS and software development with ease and drive growth. Try it now and take your projects to new heights.`,
+    title: 'Not Found'
   } 
 const NotFound = () => {
   return (
@@ -31,21 +30,11 @@ const NotFound = () => {
           />
         </div>
         <div className="text-wrapper order-lg-first">
-          <div className="logo">
-            <Link href="/">
-              <Image width={136} 
-              height={43} src="/images/logo/deski_05.svg" alt="logo" />
-            </Link>
-          </div>
           <h1 className="font-slab">
             Sorry, <br />
             The Page Can’t <br />
             be Found.
           </h1>
-          <p className="font-rubik">
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo consequat
-          </p>
 
           <Link
             href="/"
