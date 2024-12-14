e48ca3dc455cd608b303bbd99ecb43ea9cdcf553
rfounds <r2founds@gmail.com>
2024-12-14 01:06:37 -0600

    Fixed about copy

--- components/about/About.jsx
+++ components/about/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div className="row align-items-center">
       <div className="col-lg-6" data-aos="fade-right" data-aos-duration="1200">
-        <Image width={550} height={649} style={{height:'fit-content'}}     src="/images/livingroom.jpg" alt="about" className="m-auto" />
+        <Image width={550} height={649} style={{ height: 'fit-content' }} src="/images/livingroom.jpg" alt="about" className="m-auto" />
       </div>
       {/* End .col */}
 
@@ -13,14 +13,14 @@ const About = () => {
         <div className="text-wrapper">
           <div className="title-style-five">
             <h2>
-              <span>About Us</span> 
+              <span>About Us</span>
             </h2>
           </div>
           <p className="font-rubik">
             Welcome to Foundwell Services, your premier partner in sub-leasing and real estate-focused cleaning solutions. At Foundwell Services, we understand the pivotal role cleanliness plays in presenting properties at their best.
           </p>
           <p className="font-rubik">
-            Whether you're a real estate agent, property manager, or homeowner preparing to list a property, our dedicated team is here to ensure every space shines.We aim to answer any query in less than 10 minutes.
+            Whether you're a real estate agent, property manager, or homeowner preparing to list a property, our dedicated team is here to ensure every space shines.We aim to answer any query in less than 24 hours.
           </p>
           <div className="name font-slab">Emily Founds, Owner</div>
         </div>
